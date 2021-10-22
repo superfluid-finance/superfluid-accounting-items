539ea1025e7ab6d8b689d87704cb9016bb57cee7
marten <marten.jaago@koemo.ee>
2021-10-22 17:08:46 +0300

    dev -> feature on URLs, still needs authentication

--- src/helper.js
+++ src/helper.js
@@ -5,13 +5,13 @@ const fetch = require('cross-fetch');
 
 const networkGraphUrls = new Map();
 networkGraphUrls.set(LOCAL, 'http://localhost:8000/subgraphs/name/superfluid-test');
-networkGraphUrls.set(MATIC, 'https://thegraph.com/hosted-service/subgraph/superfluid-finance/protocol-dev-matic');
-networkGraphUrls.set(XDAI, 'https://thegraph.com/hosted-service/subgraph/superfluid-finance/protocol-dev-xdai');
-networkGraphUrls.set(GOERLI, 'https://thegraph.com/hosted-service/subgraph/superfluid-finance/protocol-dev-goerli');
-networkGraphUrls.set(KOVAN, 'https://thegraph.com/hosted-service/subgraph/superfluid-finance/protocol-dev-kovan');
-networkGraphUrls.set(RINKEBY, 'https://thegraph.com/hosted-service/subgraph/superfluid-finance/protocol-dev-rinkeby');
-networkGraphUrls.set(ROPSTEN, 'https://thegraph.com/hosted-service/subgraph/superfluid-finance/protocol-dev-ropsten');
-networkGraphUrls.set(MUMBAI, 'https://thegraph.com/hosted-service/subgraph/superfluid-finance/protocol-dev-mumbai');
+networkGraphUrls.set(MATIC, 'https://thegraph.com/hosted-service/subgraph/superfluid-finance/protocol-feature-matic');
+networkGraphUrls.set(XDAI, 'https://thegraph.com/hosted-service/subgraph/superfluid-finance/protocol-feature-xdai');
+networkGraphUrls.set(GOERLI, 'https://thegraph.com/hosted-service/subgraph/superfluid-finance/protocol-feature-goerli');
+networkGraphUrls.set(KOVAN, 'https://thegraph.com/hosted-service/subgraph/superfluid-finance/protocol-feature-kovan');
+networkGraphUrls.set(RINKEBY, 'https://thegraph.com/hosted-service/subgraph/superfluid-finance/protocol-feature-rinkeby');
+networkGraphUrls.set(ROPSTEN, 'https://thegraph.com/hosted-service/subgraph/superfluid-finance/protocol-feature-ropsten');
+networkGraphUrls.set(MUMBAI, 'https://thegraph.com/hosted-service/subgraph/superfluid-finance/protocol-feature-mumbai');
 
 function getClientForNetwork(networkName) {
 	const networkUrl = networkGraphUrls.get(networkName.toLowerCase());
